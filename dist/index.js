6bb968dd8e95d69d4a1346bd13dcc1aac1d39ed0
sakurai-ryo <sakurairyo.10do@gmail.com>
2021-06-10 22:55:54 +0900

    tmp

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,23 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -9,33 +28,83 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const readline = require("readline");
+const fs = __importStar(require("fs"));
+// import * as yesno from "yesno";
+const AWS = __importStar(require("aws-sdk"));
+const diff = __importStar(require("@aws-cdk/cloudformation-diff"));
 class Plugin {
     constructor(serverless, options) {
         this.serverless = serverless;
         this.option = options;
+        this.region = options.region || this.serverless.service.provider.region;
+        this.stage = options.stage || this.serverless.service.provider.stage;
+        this.stackName = `${this.serverless.service.service}-${this.stage}`;
+        this.cf = new AWS.CloudFormation({
+            region: this.region,
+        });
         this.hooks = {
-            // tsconfig.jsonで "compilerOptions.strictBindCallApply == false" にしないと、トランスパイル時にここでエラーが起きる
-            "before:package:createDeploymentArtifacts": this.run.bind(this),
+            // "after:package:createDeploymentArtifacts": this.run.bind(this),
+            "before:deploy:deploy": this.run.bind(this),
         };
     }
     run() {
         return __awaiter(this, void 0, void 0, function* () {
-            const repl = () => __awaiter(this, void 0, void 0, function* () {
-                const rl = readline.createInterface({
-                    input: process.stdin,
-                    output: process.stdout,
-                });
-                return new Promise((resolve) => {
-                    rl.question("Please enter names for your project: ", (answer) => {
-                        console.log(`Thank you!! Let's start ${answer}`);
-                        rl.close();
-                        resolve();
-                    });
-                });
-            });
-            yield repl();
+            const oldTemp = yield this.getOldTemplate();
+            const newTemp = this.getNewTemplate();
             this.serverless.cli.log("=== sample log ===");
+            console.log("Stack: ", this.stackName);
+            const structDatas = [
+                {
+                    handler: "http",
+                    endpoint: "http://localhost:3000/path",
+                    method: "ALL",
+                },
+                {
+                    handler: "event",
+                    endpoint: "http://localhost:3000/event",
+                    method: "POST",
+                },
+                { handler: "GCS", endpoint: "http://localhost:3000/GCS", method: "POST" },
+            ];
+            console.table(structDatas);
+            this.calcDiffs(oldTemp, newTemp);
+        });
+    }
+    // private async repl(): Promise<boolean> {
+    //   const ok = await yesno({
+    //     question: "Dude, Is this groovy or what?",
+    //     yesValues: ["Y"],
+    //     noValues: ["N"],
+    //   });
+    //   return ok;
+    // }
+    isFirstDeploy() {
+        const files = fs.readdirSync("./.serverless");
+        if (files.includes("cloudformation-template-update-stack.json"))
+            return false;
+        else
+            return true;
+    }
+    getOldTemplate() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const temp = yield this.cf
+                .getTemplate({
+                StackName: this.stackName,
+                TemplateStage: "Processed",
+            })
+                .promise();
+            return JSON.parse(temp.TemplateBody);
+        });
+    }
+    getNewTemplate() {
+        return JSON.parse(fs.readFileSync("./.serverless/cloudformation-template-update-stack.json", "utf8"));
+    }
+    calcDiffs(oldTemp, newTemp) {
+        const diffs = diff.diffTemplate(oldTemp, newTemp);
+        Object.keys(diffs.resources.changes).forEach((key) => {
+            Object.keys(diffs.resources.changes[key].propertyUpdates).forEach((k) => {
+                console.log(`${key}: `, diffs.resources.changes[key].propertyUpdates[k]);
+            });
         });
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import * as Serverless from "serverless";
 
-import * as readline from "readline";
+import * as fs from "fs";
+// import * as yesno from "yesno";
+import * as AWS from "aws-sdk";
+import * as diff from "@aws-cdk/cloudformation-diff";
+
+type Template = {
+  AWSTemplateFormatVersion: string;
+  Description?: string;
+  Resources: any;
+  Outputs: any;
+};
 
 export default class Plugin {
   public serverless: Serverless;
@@ -8,32 +18,92 @@ export default class Plugin {
   public hooks: {
     [event: string]: Promise<any>;
   };
+  private cf: AWS.CloudFormation;
+  private region: string;
+  private stage: string;
+  private stackName: string;
 
   constructor(serverless: Serverless, options: Serverless.Options) {
     this.serverless = serverless;
     this.option = options;
+    this.region = options.region || this.serverless.service.provider.region;
+    this.stage = options.stage || this.serverless.service.provider.stage;
+    this.stackName = `${this.serverless.service.service}-${this.stage}`;
+    this.cf = new AWS.CloudFormation({
+      region: this.region,
+    });
 
     this.hooks = {
-      "before:package:createDeploymentArtifacts": this.run.bind(this),
+      // "after:package:createDeploymentArtifacts": this.run.bind(this),
+      "before:deploy:deploy": this.run.bind(this),
     };
   }
 
   public async run() {
-    await this.repl();
+    const oldTemp = await this.getOldTemplate();
+    const newTemp = this.getNewTemplate();
     this.serverless.cli.log("=== sample log ===");
+    console.log("Stack: ", this.stackName);
+    const structDatas = [
+      {
+        handler: "http",
+        endpoint: "http://localhost:3000/path",
+        method: "ALL",
+      },
+      {
+        handler: "event",
+        endpoint: "http://localhost:3000/event",
+        method: "POST",
+      },
+      { handler: "GCS", endpoint: "http://localhost:3000/GCS", method: "POST" },
+    ];
+    console.table(structDatas);
+    this.calcDiffs(oldTemp, newTemp);
   }
 
-  private async repl() {
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
+  // private async repl(): Promise<boolean> {
+  //   const ok = await yesno({
+  //     question: "Dude, Is this groovy or what?",
+  //     yesValues: ["Y"],
+  //     noValues: ["N"],
+  //   });
+  //   return ok;
+  // }
 
-    return new Promise((resolve) => {
-      rl.question("Please enter names for your project: ", (answer) => {
-        console.log(`Thank you!! Let's start ${answer}`);
-        rl.close();
-        resolve();
+  private isFirstDeploy(): boolean {
+    const files = fs.readdirSync("./.serverless");
+    if (files.includes("cloudformation-template-update-stack.json"))
+      return false;
+    else return true;
+  }
+
+  private async getOldTemplate(): Promise<Template> {
+    const temp = await this.cf
+      .getTemplate({
+        StackName: this.stackName,
+        TemplateStage: "Processed",
+      })
+      .promise();
+    return JSON.parse(temp.TemplateBody);
+  }
+
+  private getNewTemplate(): Template {
+    return JSON.parse(
+      fs.readFileSync(
+        "./.serverless/cloudformation-template-update-stack.json",
+        "utf8"
+      )
+    );
+  }
+
+  private calcDiffs(oldTemp: Template, newTemp: Template) {
+    const diffs = diff.diffTemplate(oldTemp, newTemp);
+    Object.keys(diffs.resources.changes).forEach((key) => {
+      Object.keys(diffs.resources.changes[key].propertyUpdates).forEach((k) => {
+        console.log(
+          `${key}: `,
+          diffs.resources.changes[key].propertyUpdates[k]
+        );
       });
     });
   }
